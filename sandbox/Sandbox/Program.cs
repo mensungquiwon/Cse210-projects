ee79387a9528f7066ee1e11876a64454b2c1cb54
mensungqquiwon <jquiwon@byupathway.edu>
2026-01-07 11:50:10 +0000

    run my fist c# program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!!!");
+        Console.WriteLine("This is a C# program.");
+
+        int number = 34;
+        number = number + 6;
+        Console.WriteLine("The result is: " + number);
+
+        Console.Write("what is your name? ");
+        string name = Console.ReadLine();
+        Console.Write($"Your name is {name}.");
+
     }
 }
--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask user for their name and greet them
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine(); 
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");  
+
     }
 }
