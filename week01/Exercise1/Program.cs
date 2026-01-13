42e853a3a12e42381a35d165931323603f053780
mensungqquiwon <jquiwon@byupathway.edu>
2026-01-13 11:20:28 +0000

    Applied abstraction principles.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,18 +1,75 @@
 using System;
 
-class Program
+namespace MyApp
 {
-    static void Main(string[] args)
+    class Blind
     {
-        // Ask user for their name and greet them
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        public double _width;
+        public double _height;
+        public string _color;
+         public double GetArea()
+        {
 
-        Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine();
-        Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine(); 
+            return _width * _height;
+        }
+        public Blind()
+        {
 
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");  
-
+        }
+    public class House
+        {
+            public string _owner = "";
+            public Blind _kitchen = new Blind();
+            public Blind _window = new Blind();
+        }
+        
     }
-}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House quiwonHome = new House();
+            quiwonHome._owner = "Quiwon Family";
+
+            quiwonHome._window._width = 120.5;
+            quiwonHome._window._height = 200.0;
+            quiwonHome._window._color = "Blue";
+
+            double materialAmount1 = quiwonHome._window.GetArea();
+            Console.WriteLine($"Window's area: {materialAmount1}cm");
+            
+            Blind kitchen = new Blind();
+            kitchen._width = 150.0;
+            kitchen._height = 250.0;
+            kitchen._color = "Red";
+
+            double materialAmount = kitchen.GetArea();
+
+            Console.WriteLine($"kitchen's area: {materialAmount}cm");
+
+
+
+        }
+       }
+    }
+
+
+
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         // Ask user for their name and greet them
+//         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+//         Console.Write("What is your first name? ");
+//         string firstName = Console.ReadLine();
+//         Console.Write("What is your last name? ");
+//         string lastName = Console.ReadLine(); 
+
+//         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");  
+
+//     }
+// }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "DuxOrigin";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+        // job1.DisplayJob();
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Ux Designer";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+        // job2.DisplayJob();
+
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Cynthia Morris";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
+
     }
 }
